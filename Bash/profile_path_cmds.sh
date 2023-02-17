170e4c04fb389fa7218ef5c3ae6acb4f0896a9d2
jurtasunel <114501562+jurtasunel@users.noreply.github.com>
2023-02-17 12:46:42 +0000

    Update profile_path_cmds.sh

--- Bash/profile_path_cmds.sh
+++ Bash/profile_path_cmds.sh
@@ -8,3 +8,6 @@
 # To add new path toPATH, at the end write:
 # export PATH=/path/to/newpath:$PATH
 # Always write to the left of the path, it will use the first match.
+
+# Example to add new blast version.
+#export PATH=/home/josemari/ncbi-blast-2.13.0+/bin:$PATH
