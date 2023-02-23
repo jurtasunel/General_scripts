3864b89ad53fac6db833cb9ed43409c1843badb9
jurtasunel <114501562+jurtasunel@users.noreply.github.com>
2023-02-23 14:57:08 +0000

    Update phylotree_from_msa.R

--- R/phylotree_from_msa.R
+++ R/phylotree_from_msa.R
@@ -12,25 +12,46 @@ aln <- read.dna(fasta_path, format = "fasta")
 reference <- aln[1,]
 reference_label <- rownames(reference)
 
-# Make a tree using neighbor-joining method
+# Make a tree using neighbor-joining method.
 tree <- nj(dist.dna(aln))
 # Root the tree to the reference.
 rooted_tree <- RootTree(tree, reference_label)
 
-# Get the tip labels
+# Calculate bootstrap values.
+bs_replicates = 1000
+bs <- boot.phylo(rooted_tree, aln, FUN = function(xx) nj(dist.dna(xx)), B = bs_replicates, rooted = TRUE)
+# Adjust to percentage for 1000 bootstrap replicates.
+bs <- bs/10
+# Loop through the bootstraps.
+for (i in 1:length(bs)){
+  
+  # Replace NA by empty string.
+  if (is.na(bs[i]) == TRUE){
+    bs[i] <- ""
+    # Add the % symbol. 
+  } else{
+    bs[i] <- paste0(bs[i], "%")}
+  }
+# Round them, add % symbol and add them to the tree as node labels.
+rooted_tree$node.label <- bs
+
+# Get the tip labels.
 labels <- rownames(aln)
 
-# Colour the reference blue and the other sequences red
+# Colour the reference blue and the other sequences red.
 tip.colours <- c("darkblue", rep("darkred", (length(labels) - 1)))
 
-# Calculate pairwise distances between tips
+# Calculate pairwise distances between tips.
 distances <- cophenetic(rooted_tree)
 # Make a sequence between the minimun and maximun for plotting evenly spaced axis tips.
 dist_axes <- round(seq(min(distances), max(distances), by = max(distances)/5), 3)
 
-# Plot the tree with coloured tip labels and genetic distance on x-axis
-plot(rooted_tree, tip.color = tip.colours, cex = 0.75, align.tip.label = TRUE)
+# Plot the tree with coloured tip labels and showing bootstraps.
+tree_plot <- plot(rooted_tree, tip.color = tip.colours, cex = 0.75, align.tip.label = TRUE, show.node.label = TRUE)
+# Show genetic distance on x-axis.
 axis(side = 1, at = dist_axes, labels = TRUE, tick = TRUE, xlim = range(distances), cex.axis = 0.8)
+# Add margin text.
+mtext(paste0("*Number of bootstrap replicates: ", bs_replicates), side = 3, cex = 0.8)
 
 
 
