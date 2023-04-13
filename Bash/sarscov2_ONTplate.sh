ae3ec4ee45d64cc35b822d6d318ef2e74c6dc028
jurtasunel <114501562+jurtasunel@users.noreply.github.com>
2023-04-13 16:37:02 +0100

    Update sarscov2_ONTplate.sh

--- Bash/sarscov2_ONTplate.sh
+++ Bash/sarscov2_ONTplate.sh
@@ -3,6 +3,7 @@
 # On one terminal window, ssh to GridION.
 ssh grid@$GRU
 # Go to data folder. Plate name is the same as the one on the NVRL SARS COV 2 group.
+# *** go to root with: cd / (NOT TO HOME, which is cd ~)
 cd /data/{run_date}/{plate_name}
 # In plate name there is a folder with long name, then several folders including fastq_pass.
 cd */fasq_pass
@@ -24,8 +25,25 @@ my.getNVRL.run.sh 120423 040423_SEQ_RUN1_COSQ GRU 35
 # Copy file to INSEQUENCES folder and:
 # 	change name to NVRL.{plate_name}.plate.txt
 # 	replace all spaces with underscores
-# Copy colums to metadata file and index link the barcodes with the metadata by the NVRL codes.
+# Copy colums to metadata file
+# Index link the barcodes with the metadata by the NVRL codes, which is the second column of the plate.txt map file.
 
-# Metadata file names?
+# If there are empty rows with NVRL code, autocomplete rows with the formula.
+# check that there are no NA or missed values etc. 
+
+# save AS CSV with name NVRL.{one/two/trww}plate_date.metadata.csv}
+# MAKE SURE IS SAVED AS CSV
+# save it on INSEQUENCES, where the plate map file also is. 
+
+# Check disk space.
+df -h
+# mounted on "/" --> that is root
+# on GRU/DRU, check the folder "/data" has always space 
+
+# Check how much space takes one specific folder.
+du -h {folder_name}
+
+# shows all memory, but also does pagination
+cat /proc/meminfo # ram is the first row, "MemTotal"
 
 
