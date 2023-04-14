6f6424bcbc5d4d07103a4c144da941a6e1e640fb
jurtasunel <114501562+jurtasunel@users.noreply.github.com>
2023-04-14 13:31:58 +0100

    Add files via upload

---
+++ Bash/format_external_disk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Format disks to have NTFS (Windows compatible and files biger tan 4GB) instead of FAT32(old version).
+# 1. Empty disk.
+# 2. Go to disks > stop dist > configure > check file system to see format.
+# 3. Format volume > Choose option "For use with Windows (NTFS)
+
+# Decompress tar files.
+#tar -xf {file_name}
